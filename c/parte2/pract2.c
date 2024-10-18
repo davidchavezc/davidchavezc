d014ce0273ba1bcfd89918015b22c4b435fcdfed
David <bernardo.chavezc@uanl.edu.mx>
2024-10-17 20:38:52 -0600

    Ejercicios de la clase de Taller de Programación

---
+++ c/parte2/pract2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+	int limite;
+	printf("Ingrese el limite de la serie: "); scanf("%i",&limite);
+	printf("Limite: %d \n", limite);
+	for (int cont=1;limite>=cont;cont++){
+		for (int alt=1;cont>=alt;alt++){
+			printf("%d ", alt);
+		}
+		printf("\n");
+	}
+}
