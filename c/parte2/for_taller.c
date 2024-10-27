956e9e4b1b79f28666842cf81fefede2ce3ecebd
davidchavezc <davidchavez1805@outlook.com>
2024-10-27 16:16:54 -0600

    Delete old c folder

--- c/parte2/for_taller.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(){
-    int x = 5, i = 1;
-    while(i<=x){
-        i++;
-        printf("Hola \n");
-    }
-    return 0;
-}
--- c/parte2/return100.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main(){
-    int i;
-    for (i=1;100>=i;i++){ 
-        printf("Ciclo: %d \n", i);
-    }
-    return 0;
-}
