890b5abbea0f775021e878e5d013599050f65551
davidchavezc <davidchavez1805@outlook.com>
2024-11-06 01:07:07 -0600

    Add matriz funcitionality

---
+++ taller-progra/array/matriz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+	int tamano;
+	printf("Indique el tamaño de la matriz: "); scanf("%d", &tamano);
+	int array[tamano][tamano];
+	for(int i=0;i<tamano;i++)
+	{
+		for(int j=0;j<tamano;j++)
+		{
+			printf("\nIngrese la posición [%d][%d] de la matriz: ", i, j);
+			scanf("%d", &array[i][j]); 
+		}
+	}
+	printf("\nMatriz: \n");
+	for (int i=0;i<tamano;i++)
+	{
+		for(int j=0;j<tamano;j++)
+		{
+			printf("%d ",array[i][j]);
+		}
+		printf("\n");
+	}
+}
