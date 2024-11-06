6b5eefc989d4ce3e26c6e989eb3602defb1ba8bd
davidchavezc <davidchavez1805@outlook.com>
2024-11-06 01:23:15 -0600

    Add function for showing main diag

--- taller-progra/array/matriz.c
+++ taller-progra/array/matriz.c
@@ -29,6 +29,24 @@ int main()
 			suma+=array[i][j];
 		}
 	}
-	printf("La suma de los elementos de la matriz es: %d\n", suma);
+	printf("\nLa suma de los elementos de la matriz es: %d", suma);
+	printf("\nEl promedio de los elementos de la matriz es: %d \n", suma/tamano/tamano);
+	printf("\nDiagonal principal: \n");
+	for(int i=0;i<tamano;i++)
+	{
+		for(int j=0;j<tamano;j++)
+		{
+			if(i==j)
+			{
+				printf("%d ", array[i][j]);
+			}
+			else
+			{
+				printf("0 ");
+			}
+		}
+		printf("\n");
+	}
+	printf("\n");
 	return 0;
 }
